e5abe55fa7f3ab1837b4f223020c593d3a1bc6ee
dmjoyce92 <dmjoyce92@gmail.com>
2017-02-02 17:12:22 -0500

    Nav menu is no longer displayed in the background after being collapsed

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 	$('.mobile-menu').on('click', function(e) {
 		$('.loaded').toggleClass('nav-visible');
-		$('.nav-mobile').removeClass('hidden');
-		e.stopPropagation();
 	});
+  var action = 1;
+  $('.mobile-menu').on("click", viewSomething);
+  function viewSomething() {
+    if ( action == 1 ) {
+        $('.nav-mobile').removeClass('collapse');
+        action = 2;
+    } else {
+			  setTimeout(function(){
+				  $('.nav-mobile').addClass('collapse');
+			  }, 500);
+        action = 1;
+    }
+  }
 });
